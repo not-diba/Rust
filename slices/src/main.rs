ae368591271c4c4dec361e739cce73df374ebada
not-diba <elvis.madiba@gmail.com>
2026-04-24 00:07:11 +0300

    feat: Memory ownership & slices

---
+++ slices/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let s = String::from("Hello world sth");
+    let word = first_word(&s);
+    println!("the fisrt word is {word}");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}

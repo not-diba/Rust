6b7d1a7837279592d025de09cc83c46cdea42ed2
not-diba <elvis.madiba@gmail.com>
2026-05-07 19:13:43 +0300

    feat: generics

---
+++ generics/src/main.rs
@@ -0,0 +1,50 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
+}
+
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    // let both_integer = Point { x: 5, y: 10 };
+    // let both_float = Point { x: 1.0, y: 4.0 };
+    let p = Point { x: 5, y: 4.0 };
+
+    println!("p.x = {}", p.x());
+    println!("p.y = {}", p.y());
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
+}
